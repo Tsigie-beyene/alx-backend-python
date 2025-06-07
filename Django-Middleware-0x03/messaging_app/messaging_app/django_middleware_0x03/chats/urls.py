5661ae6e8fdb85476f1f9fa659b25fe41adc4e9a
Tsigie Beyene <119432900+Tsigie-beyene@users.noreply.github.com>
2025-06-07 11:47:10 -0700

    Create urls.py

---
+++ Django-Middleware-0x03/messaging_app/messaging_app/django_middleware_0x03/chats/urls.py
@@ -0,0 +1,21 @@
+from rest_framework_nested import routers
+from django.urls import path, include
+from .views import ConversationViewSet, MessageViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+router = routers.DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+
+# Nested router for messages under conversations
+conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(conversations_router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
